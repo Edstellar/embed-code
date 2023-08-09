86a38c873d891540f7a88157b594b1d1d034d928
subhash <info@edstellar.com>
2023-08-09 19:12:29 +0530

    added new html

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,13 +1,5 @@
 import { useState } from "react";
-import {
-  TextInput,
-  Checkbox,
-  Button,
-  Group,
-  Box,
-  Text,
-  Code,
-} from "@mantine/core";
+import { TextInput, Button, Group, Box, Text, Code } from "@mantine/core";
 import { useForm } from "@mantine/form";
 
 function Dashboard() {
@@ -36,9 +28,21 @@ function Dashboard() {
         ? link?.blog
         : `https://${link?.blog}`;
     return `
+    <div class="copy-div">Embed this infographic on your site:
+<button class="copy-btn" title="Copy to clipboard" data-copy-text="<p style="max-width: 600px; overflow: auto;"><a style="max-width: 600px" href="${blog}" _rte_href="${blog}" target="_blank"> <img style="max-width: 600px" src="${link?.link}" _rte_src="${link?.link}" alt="${link?.alt}" width="600px" border="0" /> </a> </p> <p>Via <a href="https://www.edstellar.com" _rte_href="https://www.edstellar.com" target="_blank">Edstellar</a> </p>">Copy to clipboard</button></div>
+    `;
+  };
+
+  const codeBlock2 = (link) => {
+    const blog =
+      link?.blog.startsWith("https://") || link?.blog.startsWith("http://")
+        ? link?.blog
+        : `https://${link?.blog}`;
+    return `
     <p style="max-width: 600px; overflow: auto;"><a style="max-width: 600px" href="${blog}" _rte_href="${blog}" target="_blank"> <img style="max-width: 600px" src="${link?.link}" _rte_src="${link?.link}" alt="${link?.alt}" width="600px" border="0" /> </a> </p> <p>Via <a href="https://www.edstellar.com" _rte_href="https://www.edstellar.com" target="_blank">Edstellar</a> </p>
     `;
   };
+
   return (
     <>
       <Box maw={300} mx="auto">
@@ -59,7 +63,7 @@ function Dashboard() {
           />
           <TextInput
             withAsterisk
-            label="alt Link"
+            label="alt text"
             placeholder="enter alt text"
             mb={10}
             {...form.getInputProps("alt")}
@@ -73,7 +77,7 @@ function Dashboard() {
       {link && (
         <>
           <Text component="h2">preview</Text>
-          <Box dangerouslySetInnerHTML={{ __html: codeBlock(link) }}></Box>
+          <Box dangerouslySetInnerHTML={{ __html: codeBlock2(link) }}></Box>
         </>
       )}
       <Box maw={750} mx={"auto"} mt={30}>
